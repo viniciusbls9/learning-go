b83a64ed86c5b83749fd7b1adf1467b141d44665
Vinicius <beneditoloura@gmail.com>
2023-06-27 07:40:37 -0300

    create CreateCar func

--- main.go
+++ main.go
@@ -10,20 +10,30 @@ type Car struct {
 // slice: like an infinit array
 var cars []Car
 
-func createCars () {
+func generateCars () {
 	cars = append(cars, Car{Name: "Ferrari", Price: 100})
 	cars = append(cars, Car{Name: "Mercedes", Price: 200})
 	cars = append(cars, Car{Name: "Porsche", Price: 300})
 }
 
 func main() {
-	createCars()
+	generateCars()
 	e := echo.New()
 	e.GET("/cars", getCars)
+	e.POST("/cars", createCar)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 
 func getCars(c echo.Context) error {
+	return c.JSON(200, cars)
+}
+
+func createCar (c echo.Context) error {
+	car := new(Car)
+	if err := c.Bind(car); err != nil {
+		return err
+	}
+	cars = append(cars, *car)
 	return c.JSON(200, cars)
 }
